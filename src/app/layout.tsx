905dd1f0e773d72e9594cbbcea82428dd89f94b7
niadulMunna <niadulislammunna@gmail.com>
2025-01-03 23:39:53 +0600

    footer imported

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
+import Footer from "@/components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -40,6 +41,7 @@ export default function RootLayout({
             <Navbar />
           </header>
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
